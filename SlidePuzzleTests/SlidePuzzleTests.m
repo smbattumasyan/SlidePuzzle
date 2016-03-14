1e295afaad8338439ce2cbbf58c82e13354c1e09
Smbat Tumasyan <smbatt@davitg-mac.local>
2016-03-14 17:09:18 +0400

    Initial Commit

---
+++ SlidePuzzleTests/SlidePuzzleTests.m
@@ -0,0 +1,39 @@
+//
+//  SlidePuzzleTests.m
+//  SlidePuzzleTests
+//
+//  Created by Smbat Tumasyan on 3/14/16.
+//  Copyright © 2016 EGS. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SlidePuzzleTests : XCTestCase
+
+@end
+
+@implementation SlidePuzzleTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
